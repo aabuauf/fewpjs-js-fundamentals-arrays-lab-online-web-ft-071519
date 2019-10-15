f18748696bc201d5b32e0f3cde9accccabec7096
Ahmed Abououf <aabuauf@yahoo.com>
2019-10-15 12:01:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // Write your solution here!
-const  appent = 
+const  appent = ["Milo", "Otis", "Garfield"]
+const prepend = ["Milo", "Otis", "Garfield"]
+const removeLast = ["Milo", "Otis", "Garfield"]
+const remove First = ["Milo", "Otis", "Garfield"]
